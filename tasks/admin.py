c6f37da4503031457b52dc36d7afff409354855e
CSEBASTIA <sedas7778@gmail.com>
2026-01-20 22:28:16 -0500

    actualizado a hoja de vida

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,7 +1,17 @@
 from django.contrib import admin
-from .models import Task
+from .models import Skill, Project, Education
 
-class TaskAdmin(admin.ModelAdmin):
-    readonly_fields = ('created',)
+@admin.register(Skill)
+class SkillAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category', 'icon')
+    list_filter = ('category',)
 
-admin.site.register(Task, TaskAdmin)
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ('title', 'company', 'date')
+    search_fields = ('title', 'company')
+
+@admin.register(Education)
+class EducationAdmin(admin.ModelAdmin):
+    list_display = ('title', 'institution', 'date')
+    search_fields = ('title', 'institution')
--- tasks/models.py
+++ tasks/models.py
@@ -1,13 +1,43 @@
 from django.db import models
-from django.contrib.auth.models import User
 
-class Task(models.Model):
-    title = models.CharField(max_length=200)
-    description = models.TextField(blank=True)
-    created = models.DateTimeField(auto_now_add=True)
-    datecompleted = models.DateTimeField(null=True, blank=True)
-    important = models.BooleanField(default=False)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-
+class Skill(models.Model):
+    CATEGORY_CHOICES = [
+        ('backend', 'Backend'),
+        ('frontend', 'Frontend'),
+        ('tools', 'Herramientas'),
+    ]
+    
+    name = models.CharField(max_length=100)
+    category = models.CharField(max_length=20, choices=CATEGORY_CHOICES)
+    icon = models.CharField(max_length=10, default='⚙️')
+    
     def __str__(self):
-        return self.title + ' - by ' + self.user.username
+        return self.name
+    
+    class Meta:
+        ordering = ['category', 'name']
+
+
+class Project(models.Model):
+    title = models.CharField(max_length=200)
+    company = models.CharField(max_length=200)
+    date = models.CharField(max_length=50)
+    description = models.TextField()
+    
+    def __str__(self):
+        return self.title
+    
+    class Meta:
+        ordering = ['-date']
+
+
+class Education(models.Model):
+    title = models.CharField(max_length=200)
+    institution = models.CharField(max_length=200)
+    date = models.CharField(max_length=50)
+    
+    def __str__(self):
+        return self.title
+    
+    class Meta:
+        ordering = ['-date']
